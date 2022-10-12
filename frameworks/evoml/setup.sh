30833800efd7308988d207897908ed57c3648244
vitali87 <vitali@turintech.ai>
2022-10-12 17:22:35 +0100

    fix: bump evoml_client version

--- frameworks/evoml/setup.sh
+++ frameworks/evoml/setup.sh
@@ -19,7 +19,7 @@ echo "creating local venv "
 #${HERE}/venv/bin/pip install evoml-client	#--extra-index-url https://chatterquant:yUzy46I%5C%21eu5v@nexus.dataspartan.com/repository/pypi-external
 
 #${HERE}/venv/bin/python -m pip install evoml_client[full]==0.1.4 evoml_pipeline_interface metaml --extra-index-url https://chatterquant:yUzy46I\!eu5v@nexus.dataspartan.com/repository/pypi-external/simple
-${HERE}/venv/bin/python -m pip install metaml[all] evoml_client[full]==0.1.6 --extra-index-url https://chatterquant:yUzy46I\!eu5v@nexus.dataspartan.com/repository/pypi-external/simple
+${HERE}/venv/bin/python -m pip install metaml[all] evoml_client[full]==0.1.7 --extra-index-url https://chatterquant:yUzy46I\!eu5v@nexus.dataspartan.com/repository/pypi-external/simple
 echo 'ec DONE'
 ${HERE}/venv/bin/python -m pip install evoml_pipeline_interface --extra-index-url https://chatterquant:yUzy46I\!eu5v@nexus.dataspartan.com/repository/pypi-external/simple
 ${HERE}/venv/bin/python -m pip install evoml_utils==0.6.21 --extra-index-url https://chatterquant:yUzy46I\!eu5v@nexus.dataspartan.com/repository/pypi-external/simple
